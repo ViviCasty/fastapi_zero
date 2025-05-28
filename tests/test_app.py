5b0a551d6fd80d7a053cad268a343d9b3e1cb291
Vivi <ichpendragon@gmail.com>
2025-05-28 10:24:36 -0300

    subindo arquivo

---
+++ tests/test_app.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+client = TestClient(app)
+
+
+def test_root_deve_retornar_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA)
+    - A: Arrange - Arranjo
+    - A: Act - executa a coisa (o SUT)
+    - A: Assert - Garnat que A é A
+    """
+    # Arrange
+    client = TestClient(app)
+
+    # Act
+    response = client.get('/')
+
+    # Assert
+    assert response.json() == {'message': 'Olá Mundo'}
+    assert response.status_code == HTTPStatus.OK
