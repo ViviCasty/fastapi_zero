58a2be057dbcee3a41c2a91b22ae631cc513e45e
Vivi <ichpendragon@gmail.com>
2025-06-19 18:19:17 -0300

    Adicionando schema de menssagem

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,29 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI(title="api da vivi")
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='Api da vivi')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
+    # return "123"
     return {'message': 'Oi Momoi'}
+    # return Message(message="Pei")
+    # por padrão o fastapi sempre retorna json
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo no html </h1>
+      </body>
+    </html>"""
