d7d8883429f7d7cff149f220296e596dd4b8d100
ViviCasty <ichpendragon@gmail.com>
2025-10-02 12:42:52 -0300

    adição de mocktime adição de teste com select

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,37 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fastapi_zero.app import app
+from fastapi_zero.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
 
-from sqlalchemy import create_engine
-from fastapi_zero.models import table_registry
-from sqlalchemy.orm import Session
 
+@pytest.fixture
 def session():
     engine = create_engine('sqlite:///:memory:')
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
-        yield session
+        yield session  # sessão começa e termina / pausado
+
+    table_registry.metadata.drop_all(engine)  # deleta tabelas apos testes
+
+from sqlalchemy import event
+from fastapi_zero.models import User
+
+def _mock_db_time(model=User):
+
+    def fake_time_hook(mapper, connection, target):
+        print(target)
+    event.listen(User, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(User, 'before_insert', fake_time_hook)
 
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,26 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
 from fastapi_zero.models import User
 
 
-def test_create_user():
-    user = User(username='test', email='test@gamil.com', password='secrets')
-    assert user.username == 'test'
+def test_create_user(session):
+    new_user = User(
+        username='test', email='test@gmail.com', password='secrets'
+    )
+    session.add(new_user)  # sessão é uma transição e adiciona o user
+    session.commit()  # uma operação só/mais leve
+
+    user = session.scalar(
+        select(User).where(User.username == 'test')
+    )  # pedindo resultado da operação, retorna escalar ou um objeto python
+    # resultado da instancia
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@gmail.com',
+        'password': 'secrets',
+        'created_at': ...
+    }
